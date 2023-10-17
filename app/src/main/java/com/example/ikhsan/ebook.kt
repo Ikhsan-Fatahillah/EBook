579c650f2b9477e59c2efd9a7265bc89d8206b7e
Ikhsan-Fatahillah <ikhsanngkalu@gmail.com>
2023-10-17 14:25:30 +0700

    ebook

---
+++ app/src/main/java/com/example/ikhsan/ebook.kt
@@ -0,0 +1,11 @@
+package com.example.ikhsan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ebook : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ebook)
+    }
+}
